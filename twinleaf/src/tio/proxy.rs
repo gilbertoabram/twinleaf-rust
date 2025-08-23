84a592b5cbb8686a6ce23baa3c23800442880bff
Gilberto Abram <gilberto@tersatech.com>
2025-08-23 02:10:05 -0400

    Add DeviceTree sample parser, way to dump and log samples for full tree.

--- twinleaf/src/tio/proxy.rs
+++ twinleaf/src/tio/proxy.rs
@@ -60,6 +60,7 @@ pub struct Port {
     tx: channel::Sender<Packet>,
     rx: channel::Receiver<Packet>,
     depth: usize,
+    scope: DeviceRoute,
 }
 
 #[derive(Debug, Clone)]
@@ -84,6 +85,11 @@ pub enum RpcError {
 }
 
 impl Port {
+    /// Get the scope for this port
+    pub fn scope(&self) -> DeviceRoute {
+        self.scope.clone()
+    }
+
     /// Sends a TIO packet to this port synchronously. This call will
     /// block if the port is backed up.
     pub fn send(&self, packet: Packet) -> Result<(), SendError> {
@@ -306,7 +312,7 @@ impl Interface {
             proxy_to_client_sender,
             proxy_from_client_receiver,
             rpc_timeout,
-            scope,
+            scope.clone(),
             depth,
             forward_data,
             forward_nonrpc,
@@ -322,6 +328,7 @@ impl Interface {
             tx: client_to_proxy_sender,
             rx: client_from_proxy_receiver,
             depth: depth,
+            scope: scope,
         })
     }
 
@@ -335,14 +342,19 @@ impl Interface {
         self.new_port(None, subtree_root, usize::MAX, false, false)
     }
 
-    /// New port with default parameters for the full device tree, receiving all packets.
-    pub fn tree_full(&self) -> Result<Port, PortError> {
-        self.subtree_full(DeviceRoute::root())
+    /// New port with default parameters for a subtree, useful to probe connected devices.
+    pub fn subtree_probe(&self, subtree_root: DeviceRoute) -> Result<Port, PortError> {
+        self.new_port(None, subtree_root, usize::MAX, false, true)
     }
 
     /// New port for the full device tree, useful to probe connected devices.
     pub fn tree_probe(&self) -> Result<Port, PortError> {
-        self.new_port(None, DeviceRoute::root(), usize::MAX, false, true)
+        self.subtree_probe(DeviceRoute::root())
+    }
+
+    /// New port with default parameters for the full device tree, receiving all packets.
+    pub fn tree_full(&self) -> Result<Port, PortError> {
+        self.subtree_full(DeviceRoute::root())
     }
 
     /// New port with default parameters for the full device tree, receiving only RPCs.
